2ee4fa839a2d547e81d63d65f395797c652427b9
brorminskij <alexander.borminskij@gmail.com>
2021-05-14 10:54:19 +0200

    manifesto

---
+++ src/components/Login.jsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+
+
+export const Login = () => {
+
+    return (
+        <div>
+            <div>Username</div>
+            <input type="text" />
+            <div>Password</div>
+            <input type="password" />
+            <div>
+                </div>
+                <div>
+            <input type="button" value="Login" />
+            </div>
+        </div>
+    )
+}
